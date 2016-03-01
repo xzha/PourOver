8a31e4468374d5d1242e332c2f51f9a97f541e4e
Xiongyao Zha <xzha@purdue.edu>
2016-03-01 16:02:45 -0500

    simplified bluetooth and pwm  implementation

--- main.c
+++ main.c
@@ -1,38 +1,4 @@
 #include "pourover.h"
-#include<stdio.h>
-
-float tempValue0;
-extern float creep;
-extern float currentMeas;
-extern float oldMeas;
-
-int tareFlag = 0;
-
-void uart2_transmit(char s)
-{
-    while (U2STAbits.UTXBF);        // Wait for transmit buffer
-    while (!U2STAbits.TRMT);        // Wait for transmitter  
-    U2TXREG = s;                 // Place letter into transmit register
-}
-
-void uart2_transmit_string(char * s)
-{
-    while(*s)
-    {
-        uart2_transmit(*s++);
-    }
-}
-
-void uart2_transmit_float(float f)
-{
-    char value[15];
-    
-    sprintf(value, "%f", f);
-    
-    uart2_transmit_string(value);
-    uart2_transmit('\r');
-    uart2_transmit('\n');
-}
 
 int main()
 {
@@ -47,49 +13,16 @@ int main()
     timer_initialization();             // timer1: 1Hz
                                         // timer2: 500Hz
                                         // timer3: 500Hz
-    //bluetooth_initialization();
-    
-    DELAY_US(50);
-    
-    initialize();
+    bluetooth_initialization();
+    ls_initialization();
+    // pwm intialization
+    timer_frequency(500, 2); // pump    @ timer2
+    timer_frequency(50, 3);  // servo1  @ timer3
+                             // servo2  @ timer3 
+                             // ssrelay @ timer3
+    // pwm initialization
     
     while(1)
-    {
-        
-        if(tareFlag)
-        {
-            tareFlag = 0;
-            tare(10);
-            PORTDbits.RD10 = 0;
-        }
-        
-        currentMeas = averageWeight();
-        calcCreep();
-        
-        tempValue0 = currentMeas - creep;
-        oldMeas = currentMeas;
-        
-        uart2_transmit_float(tempValue0);
-        
-        //setScale(CALIBRATION_FACTOR);
-    }
-    
-    while(0)
-    {
-        powerUp();
-        
-        tare(5);
-        
-        tempValue0 = readData();
-        
-        tempValue0++;
-        
-        tempValue0 = readData();
-        
-        tempValue0++;
-    }
-    
-    while(0)
     {
         char bt_var_handle = -1;
 
@@ -98,7 +31,7 @@ int main()
             if (receive_flag > 0) {
                 if (buffer_check(&rx_buffer,"Connected") == 1)
                     continue;
-                if (buffer_check(&rx_buffer, "Connection End") == 1)
+                else if (buffer_check(&rx_buffer, "Connection End") == 1)
                     continue;
                 
                 // BLE received data
@@ -106,15 +39,19 @@ int main()
             }
         }
         
-        char temp[20];
-        
         // process received data (state machine?)
         switch (bt_var_handle) {
-            case 0:
+            case 0: //start_brew
+                oc_dutycycle(0, 1); // pump
+                oc_dutycycle(0, 2); // servo1
+                oc_dutycycle(0, 3); // servo2
                 break;
             case 1:
                 break;
-            case 2:
+            case 2: //brew_temp 7975652bf2a24f73a2da429ac3a83dfb
+                oc_dutycycle(50, 1); // pump
+                oc_dutycycle(15, 2); // servo1
+                oc_dutycycle(10, 3); // servo2
                 break;
             case 3:
                 break;
@@ -122,9 +59,10 @@ int main()
                 break;
             case 5:
                 break;
-            case 6:
-                int_to_hexstring(read_temperature(), temp);
-                bluetooth_shw(brew_temp.handle, temp);
+            case 6: //brew_strength dbfde0ac2cf241269759042cd13e5681
+                oc_dutycycle(25, 1); // pump
+                oc_dutycycle(10, 2);  // servo1
+                oc_dutycycle(5, 3);  // servo2
                 break;
             case 7:
                 break;
@@ -142,32 +80,6 @@ void __attribute__((__interrupt__)) _T1Interrupt(void);
 void __attribute__((__interrupt__, auto_psv)) _T1Interrupt(void)
 {
     IFS0bits.T1IF = 0;
-}
-
-int ledState = 0;
-int buttonState = 0;
-
-void __attribute__ ((__interrupt__)) _T2Interrupt(void);
-void __attribute__((__interrupt__, auto_psv)) _T2Interrupt(void)
-{
-    IFS0bits.T2IF = 0;
-    
-    int reading = PORTDbits.RD11;
-    
-    if(reading != buttonState)
-    {
-        PORTDbits.RD10 = 1;
-        
-        buttonState = reading;
-        
-        tareFlag = 1;
-    }
-}
-
-void __attribute__ ((__interrupt__)) _T3Interrupt(void);
-void __attribute__((__interrupt__, auto_psv)) _T3Interrupt(void)
-{
-    IFS0bits.T3IF= 0;
 }
 
 void __attribute__((__interrupt__)) _U1TXInterrupt(void);
@@ -200,40 +112,4 @@ void __attribute__((__interrupt__, auto_psv)) _U1RXInterrupt(void)
         if (r == '\n')
             receive_flag++;
     }
-}
-
-
-
-
-// remove me
-void __attribute__((__interrupt__)) _U2RXInterrupt(void);
-void __attribute__((__interrupt__, auto_psv)) _U2RXInterrupt(void)
-{
-    char receive2;
-    
-    IFS1bits.U2RXIF = 0;                // Clear RX Interrupt flag
-    if (U2STAbits.OERR)                 // Receive buffer overrun error
-    {
-        U2STAbits.OERR = 0;             // Clear error flag to keep UART receiving
-        return;
-    }
-    
-    if (U2STAbits.FERR)                 // Framing error
-        return;
-
-    if (U2STAbits.PERR)                 // Parity error
-        return;
-    
-    if (U2STAbits.URXDA)                // Receive Buffer Data available
-    {
-        //receive_flag2 = 0;
-        receive2 = U2RXREG;
-    }
-}
-
-void __attribute__((__interrupt__)) _U2TXInterrupt(void);
-void __attribute__((__interrupt__, auto_psv)) _U2TXInterrupt(void)
-{
-    // enabling UTXEN bit will set UxTXIF flag (DS70000582E-page 16)
-    IFS1bits.U2TXIF = 0;                // Clear TX Interrupt flag
 }
